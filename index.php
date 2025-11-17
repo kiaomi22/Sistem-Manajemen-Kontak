9e210bd9bd9540e6680e50646e2421073a43e891
Zaskia Jihan <kiahanabil@gmail.com>
2025-11-17 20:01:08 +0700

    setup: initial project structure with Tailwind template

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <script src="https://cdn.tailwindcss.com"></script>
+  <title>Tugas Pemweb - Kontak</title>
+</head>
+<body class="bg-gray-100 p-8">
+
+<h1 class="text-3xl font-bold">Sistem Kontak (Setup)</h1>
+
+</body>
+</html>
